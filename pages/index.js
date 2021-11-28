6eecf7906f327b8e726ba2fa518b45bd774a0ac6
youri <gretch552@gmail.com>
2021-11-28 19:28:39 +0100

    Add self introduction to index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>This is my implemention of the NextJS 'Learn NextJS' Tutorial.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
